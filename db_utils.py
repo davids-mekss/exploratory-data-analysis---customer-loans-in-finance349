7cc3186b7630992ed3716551f6692b55feaaa989
Davids Mekss <davids.mekss@gmail.com>
2023-12-14 16:02:07 +0000

    adds a test file for testing if .gitignore file works

---
+++ db_utils.py
@@ -0,0 +1,22 @@
+# this script will extract data used for the project from the cloud
+
+class RDSDatabaseConnector:
+    # define docstring which can be found by using help(ClassName)
+    # remember to define one for each method, function etc
+    '''
+    A class that retrieves data related to customer loans in finance from the cloud
+
+    Parameters:
+    ----------
+ 
+    Attributes:
+    ----------
+
+    Methods:
+    -------
+    '''
+
+    def __init__(self):
+        pass
+
+print("Hello World")
